e7fa192d4ab76bc4806af4af2568a75af339041f
Lochie <afterlifelochie@afterlifelochie.net>
2015-01-02 10:05:54 +1100

    Doing VM work in the AST is bad.

--- src/net/allochie/vm/jass/ast/expression/BinaryOpExpression.java
+++ src/net/allochie/vm/jass/ast/expression/BinaryOpExpression.java
@@ -19,45 +19,4 @@ public class BinaryOpExpression extends Expression {
 		return "BinaryOpExpression: " + lhs + " " + mode + " " + rhs;
 	}
 
-	@Override
-	public VMValue resolve(VMClosure closure) throws VMException {
-		VMValue v0 = lhs.resolve(closure);
-		VMValue v1 = rhs.resolve(closure);
-		Type productType = Type.findProductType(v0.type, v1.type);
-		if (productType == null)
-			throw new VMException("Can't perform operations on " + v0.type.typename + " and " + v1.type.typename);
-		VMValue product = new VMValue();
-		product.type = productType;
-		switch (mode) {
-		case ADD:
-			break;
-		case BOOLAND:
-			break;
-		case BOOLOR:
-			break;
-		case DIV:
-			break;
-		case EQUALS:
-			break;
-		case GT:
-			break;
-		case GTEQ:
-			break;
-		case LT:
-			break;
-		case LTEQ:
-			break;
-		case MUL:
-			break;
-		case NOTEQUALS:
-			break;
-		case SUB:
-			break;
-		default:
-			break;
-
-		}
-
-	}
-
 }
--- src/net/allochie/vm/jass/ast/expression/Expression.java
+++ src/net/allochie/vm/jass/ast/expression/Expression.java
@@ -1,11 +1,5 @@
 package net.allochie.vm.jass.ast.expression;
 
-import net.allochie.vm.jass.VMClosure;
-import net.allochie.vm.jass.VMException;
-import net.allochie.vm.jass.VMValue;
-
-public abstract class Expression {
-
-	public abstract VMValue resolve(VMClosure closure) throws VMException;
+public class Expression {
 
 }
