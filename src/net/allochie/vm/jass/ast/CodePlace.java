fde381cca0f9786cfc8bd0c4907d2d3f4b2a140e
Lochie <afterlifelochie@afterlifelochie.net>
2015-01-05 12:51:06 +1100

    Let codeplace track the token which created it (real).

--- src/net/allochie/vm/jass/ast/CodePlace.java
+++ src/net/allochie/vm/jass/ast/CodePlace.java
@@ -4,17 +4,19 @@ import net.allochie.vm.jass.parser.Token;
 
 public class CodePlace {
 
+	public String image;
 	public int line;
 	public int column;
 
 	public CodePlace(Token what) {
+		this.image = what.image;
 		this.line = what.beginLine;
 		this.column = what.beginColumn;
 	}
 
 	@Override
 	public String toString() {
-		return "line " + line + ", col " + column;
+		return ((image != null) ? "in section " + image : " in unknown ") + " at ine " + line + ", col " + column;
 	}
 
 }
