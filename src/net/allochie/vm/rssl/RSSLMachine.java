01987520e1801944a62de252e8c0507b85d603a6
Lochie <afterlifelochie@afterlifelochie.net>
2015-01-28 20:00:54 +1100

    Documentation and cleanup.

--- src/net/allochie/vm/rssl/RSSLMachine.java
+++ src/net/allochie/vm/rssl/RSSLMachine.java
@@ -9,6 +9,14 @@ import net.allochie.vm.rssl.api.ThreadSchedule.Schedule;
 import net.allochie.vm.rssl.ast.Identifier;
 import net.allochie.vm.rssl.debug.VoidDebugger;
 
+/**
+ * The main machine container. Includes a list of all known types to the
+ * machine, the machine's globals, functions, the top closure, the threads and
+ * the thread scheduler.
+ * 
+ * @author AfterLifeLochie
+ *
+ */
 public class RSSLMachine extends Thread {
 
 	/** List of all known VM types */
@@ -37,6 +45,16 @@ public class RSSLMachine extends Thread {
 	/** The system debugger */
 	public IDebugger debugger = new VoidDebugger();
 
+	/**
+	 * Creates a new RSSL machine.
+	 * 
+	 * @param title
+	 *            The machine's name.
+	 * @param debugger
+	 *            The debugger hook.
+	 * @param schedule
+	 *            The threading schedule object.
+	 */
 	public RSSLMachine(String title, IDebugger debugger, ThreadSchedule schedule) {
 		super(title);
 		this.debugger = debugger;
@@ -44,10 +62,24 @@ public class RSSLMachine extends Thread {
 		debugger.trace("machine.init.schedule", schedule);
 	}
 
+	/**
+	 * Sets the debugger. Any future debug traces are sent to the debugger
+	 * immediately.
+	 * 
+	 * @param what
+	 *            The debugger to set to.
+	 */
 	public void setDebugger(IDebugger what) {
 		debugger = what;
 	}
 
+	/**
+	 * Puts a thread on the machine queue for immediate execution in the next
+	 * cycle.
+	 * 
+	 * @param thread
+	 *            The thread to put.
+	 */
 	public void putThread(RSSLThread thread) {
 		debugger.trace("machine.putThread", thread);
 		synchronized (live) {
@@ -75,6 +107,9 @@ public class RSSLMachine extends Thread {
 		}
 	}
 
+	/**
+	 * Steps the machine forward one cycle.
+	 */
 	public void advance() {
 		debugger.trace("machine.advance");
 		synchronized (live) {
@@ -107,6 +142,14 @@ public class RSSLMachine extends Thread {
 		}
 	}
 
+	/**
+	 * Finds a function in the machine. If the function doesn't exist and isn't
+	 * a native function, the result is null.
+	 * 
+	 * @param invokeFunc
+	 *            The function pointer.
+	 * @return The result function or null if no such function exists.
+	 */
 	public VMFunction findFunction(VMFunctionPointer invokeFunc) {
 		if (invokeFunc == null)
 			return null;
@@ -115,11 +158,27 @@ public class RSSLMachine extends Thread {
 		return findFunction(invokeFunc.functionName);
 	}
 
+	/**
+	 * Finds a function in the machine. If the function doesn't exist and isn't
+	 * a native function, the result is null.
+	 * 
+	 * @param identifier
+	 *            The identifier.
+	 * @return The result function or null if no such function exists.
+	 */
 	public VMFunction findFunction(Identifier identifier) {
 		debugger.trace("machine.findFunction", identifier);
 		return findFunction(identifier.image);
 	}
 
+	/**
+	 * Finds a function in the machine. If the function doesn't exist and isn't
+	 * a native function, the result is null.
+	 * 
+	 * @param name
+	 *            The function name.
+	 * @return The function or null if no such function exists.
+	 */
 	public VMFunction findFunction(String name) {
 		debugger.trace("machine.findFunction", name);
 		VMFunction function = funcs.get(name);
@@ -131,16 +190,35 @@ public class RSSLMachine extends Thread {
 		return null;
 	}
 
+	/**
+	 * Find a global variable in the machine.
+	 * 
+	 * @param identifier
+	 *            The identifier.
+	 * @return The global variable.
+	 */
 	public VMVariable findGlobal(Identifier identifier) {
 		debugger.trace("machine.findGlobal", identifier);
 		return globals.get(identifier.image);
 	}
 
+	/**
+	 * Allocates a new thread on the machine.
+	 * 
+	 * @param name
+	 *            The thread's name.
+	 * @param pointer
+	 *            The function pointer.
+	 * @return The thread created.
+	 */
 	public RSSLThread allocateThread(String name, VMFunctionPointer pointer) {
 		debugger.trace("machine.allocateThread", name, pointer);
 		return new RSSLThread(this, name, global, pointer);
 	}
 
+	/**
+	 * Halts the machine immediately.
+	 */
 	public void halt() {
 		halt = true;
 		wait.notifyAll();
--- src/net/allochie/vm/rssl/RSSLThread.java
+++ src/net/allochie/vm/rssl/RSSLThread.java
@@ -18,6 +18,12 @@ import net.allochie.vm.rssl.ast.statement.ConditionalStatement;
 import net.allochie.vm.rssl.ast.statement.LoopStatement;
 import net.allochie.vm.rssl.global.TypeRegistry;
 
+/**
+ * Virtual machine thread.
+ * 
+ * @author AfterLifeLochie
+ *
+ */
 public class RSSLThread {
 
 	/** Number of operations per advance */
@@ -60,6 +66,13 @@ public class RSSLThread {
 		invokeFunc = runFunc;
 	}
 
+	/**
+	 * Runs the thread.
+	 * 
+	 * @throws VMException
+	 *             Thrown if the thread cannot find the main function or if the
+	 *             call cannot be placed on the stack.
+	 */
 	public void runThread() throws VMException {
 		machine.debugger.trace("thread.runThread", this);
 		VMFunction function = machine.findFunction(invokeFunc);
@@ -68,6 +81,17 @@ public class RSSLThread {
 		requestCall(top, function, new VMValue[0]);
 	}
 
+	/**
+	 * Runs a file and imports the globals, functions and other objects into the
+	 * local thread scope.
+	 * 
+	 * @param file
+	 *            The parsed file.
+	 * @throws VMException
+	 *             Thrown if the thread cannot copy the file data, or if the
+	 *             code includes invalid or incorrect syntax, illegal access or
+	 *             other malformed behavior.
+	 */
 	public void doFile(RSSLFile file) throws VMException {
 		machine.debugger.trace("thread.doFile", this, file);
 		for (Dec what : file.decs)
@@ -113,22 +137,54 @@ public class RSSLThread {
 		}
 	}
 
+	/**
+	 * Sets the operating frequency of the thread.
+	 * 
+	 * @param speed
+	 *            The frequency.
+	 */
 	public void setFrequency(int speed) {
 		machine.debugger.trace("thread.setFrequency", this, speed);
 		frequency = speed;
 	}
 
+	/**
+	 * Interrupts the thread. If the VM is currently advancing the frames on the
+	 * thread, the current frame will be completed and the interrupt will be
+	 * processed. If the VM is not doing anything, the interrupt is ignored.
+	 */
 	public void interrupt() {
 		machine.debugger.trace("thread.interrupt", this);
 		interrupt = true;
 	}
 
-	public VMStackFrame getCurrentFrame() throws VMException {
+	/**
+	 * Gets the current frame on the thread's stack.
+	 * 
+	 * @return The current frame on the stack, or null if the stack is empty.
+	 */
+	public VMStackFrame getCurrentFrame() {
 		if (callStack.size() == 0)
 			return null;
 		return callStack.peek();
 	}
 
+	/**
+	 * Requests the invocation of a function. The function frame is placed on
+	 * the top of the thread stack and is executed when the VM next advances the
+	 * machine.
+	 * 
+	 * @param closure
+	 *            The closure scope.
+	 * @param function
+	 *            The function to invoke.
+	 * @param args
+	 *            The arguments to provide to the function.
+	 * @throws VMException
+	 *             Any exception created when creating the function call frame,
+	 *             pushing the function call frame to the stack or when
+	 *             validating the behavior of the invocation.
+	 */
 	public void requestCall(VMClosure closure, VMFunction function, VMValue[] args) throws VMException {
 		machine.debugger.trace("thread.requestCall", this, closure, function, args);
 		if (function instanceof VMNativeFunction) {
@@ -163,6 +219,23 @@ public class RSSLThread {
 		}
 	}
 
+	/**
+	 * Requests the invocation of a function. The function frame is placed on
+	 * the top of the thread stack and is executed immediately on top of the
+	 * stack.
+	 * 
+	 * @param closure
+	 *            The closure scope.
+	 * @param function
+	 *            The function to invoke.
+	 * @param args
+	 *            The arguments to provide to the function.
+	 * @throws VMException
+	 *             Any exception created when creating the function call frame,
+	 *             pushing the function call frame to the stack, when validating
+	 *             the behavior of the invocation or when invoking the function
+	 *             on the stack.
+	 */
 	public void foreignCallImmediately(VMClosure closure, VMFunction function, VMValue[] args) throws VMException {
 		machine.debugger.trace("thread.foreignCallImmediately", this, closure, function, args);
 		VMStackFrame topFrame = getCurrentFrame();
@@ -170,6 +243,19 @@ public class RSSLThread {
 		advanceUntilFrame(topFrame);
 	}
 
+	/**
+	 * Requests the invocation of a conditional statement on the stack. The
+	 * conditional container frame is placed on the thread stack and is executed
+	 * when the VM next advances the machine.
+	 * 
+	 * @param closure
+	 *            The closure scope.
+	 * @param conditional
+	 *            The conditional statement to evaluate through.
+	 * @throws VMException
+	 *             Thrown when any condition prevents the conditional from being
+	 *             placed on the top of the execution stack.
+	 */
 	public void requestCall(VMClosure closure, ConditionalStatement conditional) throws VMException {
 		machine.debugger.trace("thread.requestCall<conditional>", this, closure, conditional);
 		VMStackFrame callframe = new VMCallFrame(closure, conditional.where, conditional.statements,
@@ -177,12 +263,42 @@ public class RSSLThread {
 		callStack.push(callframe);
 	}
 
+	/**
+	 * Requests the invocation of a loop statement on the stack. The loop
+	 * container frame is placed on the thread stack and is executed when the VM
+	 * next advances the machine.
+	 * 
+	 * @param closure
+	 *            The closure scope.
+	 * @param loop
+	 *            The conditional statement to evaluate through.
+	 * @throws VMException
+	 *             Thrown when any condition prevents the loop from being placed
+	 *             on the top of the execution stack.
+	 */
 	public void requestCall(VMClosure closure, LoopStatement loop) throws VMException {
 		machine.debugger.trace("thread.requestCall<loop>", this, closure, loop);
 		VMStackFrame callframe = new VMCallFrame(closure, loop.where, loop.statements, VMCallFrameType.LOOP);
 		callStack.push(callframe);
 	}
 
+	/**
+	 * Requests the invocation of a list of statements on the stack. The
+	 * container frame is placed on the thread stack and is executed when the VM
+	 * next advances the machine.
+	 * 
+	 * @param closure
+	 *            The closure scope.
+	 * @param where
+	 *            The source of the list.
+	 * @param statements
+	 *            The list of statements.
+	 * @param loop
+	 *            The loop mode.
+	 * @throws VMException
+	 *             Thrown when any condition prevents the list from being placed
+	 *             on the top of the execution stack.
+	 */
 	public void requestCall(VMClosure closure, CodePlace where, StatementList statements, boolean loop)
 			throws VMException {
 		machine.debugger.trace("thread.requestCall<slist>", this, closure, statements, loop);
@@ -190,21 +306,53 @@ public class RSSLThread {
 		callStack.push(callframe);
 	}
 
+	/**
+	 * Requests the resolution of an expression on the stack. The container
+	 * frame is placed on the thread stack and is executed when the VM next
+	 * advances the machine.
+	 * 
+	 * @param closure
+	 *            The closure scope.
+	 * @param expression
+	 *            The expression to evaluate.
+	 * @throws VMException
+	 *             Thrown when any condition prevents the expression resolution
+	 *             wrapper frame from being placed on the top of the execution
+	 *             stack.
+	 */
 	public void resolveExpression(VMClosure closure, Expression expression) throws VMException {
 		machine.debugger.trace("thread.resolveExpression<expression>", this, closure, expression);
 		VMExpressionCallFrame callframe = new VMExpressionCallFrame(closure, expression);
 		callStack.push(callframe);
 	}
 
+	/**
+	 * Check if the thread has died.
+	 * 
+	 * @return If the thread has died.
+	 */
 	public boolean dead() {
 		return isDead;
 	}
 
+	/**
+	 * Request a frame be placed on the top of the stack.
+	 * 
+	 * @param frame
+	 *            The frame.
+	 */
 	public void requestFrame(VMStackFrame frame) {
 		machine.debugger.trace("thread.requestFrame", this, frame);
 		callStack.push(frame);
 	}
 
+	/**
+	 * Find the most recent frame of a type on the top of the stack.
+	 * 
+	 * @param typeof
+	 *            The frame type.
+	 * @return The most recent instance of the frame.
+	 */
 	public VMCallFrame findLatestFrame(VMCallFrameType typeof) {
 		if (callStack.size() != 0)
 			for (int i = callStack.size() - 1; i >= 0; i--) {
@@ -222,6 +370,15 @@ public class RSSLThread {
 		return null;
 	}
 
+	/**
+	 * Unwind the stack to the frame specified. Leaves the frame specified on
+	 * the top of the stack.
+	 * 
+	 * @param stop
+	 *            The frame to unwind to.
+	 * @throws VMException
+	 *             Thrown when nothing is on the stack to remove.
+	 */
 	public void unwindFrames(VMStackFrame stop) throws VMException {
 		machine.debugger.trace("thread.unwindFrames", this, stop);
 		while (true) {
@@ -234,6 +391,16 @@ public class RSSLThread {
 		}
 	}
 
+	/**
+	 * Remove a single frame from the head of the stack. If the head of the
+	 * stack does not match the frame provided, an exception will be raised.
+	 * 
+	 * @param frame
+	 *            The frame to remove from the head of the stack.
+	 * @throws VMException
+	 *             Thrown when the frame on the top of the stack is not the
+	 *             frame provided as a parameter.
+	 */
 	public void removeFrame(VMStackFrame frame) throws VMException {
 		machine.debugger.trace("thread.removeFrame", this, frame);
 		VMStackFrame what = callStack.peek();
@@ -242,6 +409,16 @@ public class RSSLThread {
 		callStack.pop();
 	}
 
+	/**
+	 * Advance the machine until the frame on the top of the stack matches the
+	 * frame provided.
+	 * 
+	 * @param frame
+	 *            The frame to halt at.
+	 * @throws VMException
+	 *             Thrown when the frame cannot be matched, when the stack
+	 *             becomes empty or when any frame raises an exception.
+	 */
 	private void advanceUntilFrame(VMStackFrame frame) throws VMException {
 		machine.debugger.trace("thread.advanceUntilFrame", this, frame);
 		while (true) {
@@ -268,6 +445,13 @@ public class RSSLThread {
 		isInit = true;
 	}
 
+	/**
+	 * Steps the machine by one cycle.
+	 * 
+	 * @throws VMException
+	 *             Thrown if any frame in the execution cycle throws an
+	 *             exception.
+	 */
 	public void advance() throws VMException {
 		machine.debugger.trace("thread.advance", this);
 		if (isDead)
@@ -338,6 +522,11 @@ public class RSSLThread {
 		}
 	}
 
+	/**
+	 * Checks if the thread is currently running.
+	 * 
+	 * @return If the thread is running.
+	 */
 	public boolean running() {
 		return isInit && !isDead;
 	}
