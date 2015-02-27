989ffdd0baa363881a17b240eb58385538f4c743
Lochie <afterlifelochie@afterlifelochie.net>
2015-02-27 18:18:22 +1100

    Add scheduling docs.

--- src/net/allochie/vm/rssl/runtime/api/ThreadSchedule.java
+++ src/net/allochie/vm/rssl/runtime/api/ThreadSchedule.java
@@ -41,8 +41,34 @@ public abstract class ThreadSchedule {
 	public int getCycles() {
 		return cycles;
 	}
-	
+
+	/**
+	 * Called by the system when a thread is suspended by the user or by the
+	 * system. The thread object is provided and a list of parameters is passed
+	 * to the callee method. The callee method decides on whether the thread
+	 * should be suspended by returuning a boolean state indicating if the
+	 * thread is now suspended (true) or if the thread should not be suspended
+	 * (false).
+	 * 
+	 * @param thread
+	 *            The thread which has been requested to be suspended
+	 * @param params
+	 *            The parameters provided from the suspend call
+	 * @return If the thread should be suspended
+	 */
 	public abstract boolean suspend(RSSLThread thread, Object... params);
+
+	/**
+	 * Called by the system before a suspended thread is updated. The callee
+	 * must return a boolean which indicates if the thread should be resumed
+	 * (true) or if the thread should remain suspended (false). If the thread is
+	 * resumed, the thread will be returned to the active thread queue and will
+	 * be advanced.
+	 * 
+	 * @param thread
+	 *            The thread which has been suspended
+	 * @return If the thread should be resumed
+	 */
 	public abstract boolean resume(RSSLThread thread);
 
 	@Override
